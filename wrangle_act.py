bd9b57410164997f8462a669c41a449b9830bf75
Ozi Handicraft <ozihandicraft@gmail.com>
2020-11-17 20:48:39 -0600

    feat: add requests for image-predictions.tsv, add twitter-archive-enhanced, start twitter API

--- wrangle_act.py
+++ wrangle_act.py
@@ -20,3 +20,52 @@
 # ## 2 Tidiness Issues:
 
 # %%
+import pandas as pd
+import numpy as np
+import os
+import requests
+
+import tweepy
+from tweepy import OAuthHandler
+import json
+from timeit import default_timer as timer
+
+
+
+# %%
+twitter = pd.read_csv("data/twitter-archive-enhanced.csv")
+twitter.head(5)
+
+# %%
+twitter.info()
+
+# %% [markdown]
+# ## Tweet image predictions
+
+# %%
+file_url = "https://d17h27t6h515a5.cloudfront.net/topher/2017/August/599fd2ad_image-predictions/image-predictions.tsv"
+req = requests.get(file_url)
+fname = os.path.basename(file_url)
+open("data/" + fname, 'wb').write(req.content)
+
+# %% [markdown]
+# ## Query Twitter API for additional data
+#
+#  * retweet count
+#  * favorite count
+#  * any additional data found that's interesting
+#  * only tweets on Aug 1st, 2017 (image predictions present)
+
+# %%
+
+# %%
+consumer_key = 'HIDDEN'
+consumer_secret = 'HIDDEN'
+access_token = 'HIDDEN'
+access_secret = 'HIDDEN'
+
+auth = OAuthHandler(consumer_key, consumer_secret)
+auth.set_access_token(access_token, access_secret)
+
+api = tweepy.API(auth, wait_on_rate_limit=True)
+
