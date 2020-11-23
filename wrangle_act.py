639bdb7136bb571dab83af7a658306550fd76d67
Ozi Handicraft <ozihandicraft@gmail.com>
2020-11-23 12:50:24 -0600

    feat: gather data 1,2 done, adding data

--- wrangle_act.py
+++ wrangle_act.py
@@ -14,10 +14,15 @@
 # ---
 
 # %% [markdown]
-# # WeRateDogs Data Wrangling project
-# ## 8 Quality Issues:
+# # WeRateDogs - Udacity Data Wrangling Project 03
 #
-# ## 2 Tidiness Issues:
+#
+# ### 8 Quality Issues:
+#
+# ### 2 Tidiness Issues:
+
+# %% [markdown]
+# ## Import Libraries
 
 # %%
 import pandas as pd
@@ -32,15 +37,18 @@ from timeit import default_timer as timer
 
 
 
-# %%
-twitter = pd.read_csv("data/twitter-archive-enhanced.csv")
-twitter.head(5)
+# %% [markdown]
+# ## Gather Data #1 - Twitter archive
 
 # %%
-twitter.info()
+twitterDF = pd.read_csv("data/twitter-archive-enhanced.csv")
+twitterDF.head(5)
+
+# %%
+twitterDF.info()
 
 # %% [markdown]
-# ## Tweet image predictions
+# ## Gather Data #2 - Tweet image predictions
 
 # %%
 file_url = "https://d17h27t6h515a5.cloudfront.net/topher/2017/August/599fd2ad_image-predictions/image-predictions.tsv"
@@ -49,7 +57,8 @@ fname = os.path.basename(file_url)
 open("data/" + fname, 'wb').write(req.content)
 
 # %% [markdown]
-# ## Query Twitter API for additional data
+# ## Gather Data #3 - Query Twitter API for additional data
+# Query Twitter's API for JSON data for each tweet ID in the Twitter archive
 #
 #  * retweet count
 #  * favorite count
@@ -59,13 +68,63 @@ open("data/" + fname, 'wb').write(req.content)
 # %%
 
 # %%
-consumer_key = 'HIDDEN'
-consumer_secret = 'HIDDEN'
-access_token = 'HIDDEN'
-access_secret = 'HIDDEN'
+consumer_key = '3zhhip8Prz2WMil7d0mljLiwI'
+consumer_secret = '1aPALPmrd8xBzoovAQabAQJHRCWcijqyu4JJYMzn2S60s6T8iB'
+access_token = '175931642-unKWBdyHKcWLgt80FdW1dKFJ3wShN7125I8GsVUy'
+access_secret = 'HGRRbxzZKTfsYCiD90jGGyMdKSMuMaLmvWvv74ZDVx2z0'
 
 auth = OAuthHandler(consumer_key, consumer_secret)
 auth.set_access_token(access_token, access_secret)
 
 api = tweepy.API(auth, wait_on_rate_limit=True)
 
+
+# %%
+tweet_ids = twitterDF.tweet_id.values
+len(tweet_ids)
+
+# %%
+# Query Twitter's API for JSON data for each tweet ID in the Twitter archive
+count = 0
+fails_dict = {}
+start = timer()
+# Save each tweet's returned JSON as a new line in a .txt file
+with open('tweet_json.txt', 'w') as outfile:
+    # This loop will likely take 20-30 minutes to run because of Twitter's rate limit
+    for tweet_id in tweet_ids:
+        count += 1
+        print(str(count) + ": " + str(tweet_id))
+        try:
+            tweet = api.get_status(tweet_id, tweet_mode='extended')
+            print("Success")
+            json.dump(tweet._json, outfile)
+            outfile.write('\n')
+        except tweepy.TweepError as e:
+            print("Fail")
+            fails_dict[tweet_id] = e
+            pass
+end = timer()
+print(end - start)
+print(fails_dict)
+
+# %% [markdown]
+# ### I still do want to understand how to process the JSON 
+
+# %%
+with open('tweet.json', "r") as json_file:
+    data = json.load(json_file)
+    for tweet in data:
+        print(f"ID: {tweet['id']}")
+
+# %%
+rt_tweets = pd.read_json("tweet.json", lines=True)
+rt_tweets.head(5)
+
+# %%
+rt_tweets.columns
+
+# %%
+rt_tweets[rt_tweets.retweeted == True]
+
+# %%
+rt_tweets.iloc[]
